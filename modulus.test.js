312979a9ab3b1c3cd1e37b39cc100c5e9f0d97a7
Chukwuemeka Inya <chukwuemekai@connecterra.io>
2021-09-22 14:43:57 +0200

    Testing github secrets

--- modulus.test.js
+++ modulus.test.js
@@ -2,6 +2,6 @@ import { modulus } from "./modulus";
 
 describe("The modulus function", () => {
   it("finds the modulus", () => {
-    expect(modulus(5, 2)).toEqual(1);
+    expect(modulus(5, 2)).toEqual(2);
   });
 });
